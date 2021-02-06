e498b8bba963e89a496d00742297eebd10fc7559
Daya Sharon <daya@picktorial.com>
2021-02-06 16:05:20 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 var searchvisible = 0;
 
-$("#search-menu").click(function(e){ 
-    //This stops the page scrolling to the top on a # link.
-    e.preventDefault();
-
-    var val = $('#search-icon');
-    if(val.hasClass('ion-ios-search-strong')){
-        val.addClass('ion-ios-close-empty');
-        val.removeClass('ion-ios-search-strong');
-    }
-    else{
-         val.removeClass('ion-ios-close-empty');
-        val.addClass('ion-ios-search-strong');
-    }
-    
-    
-    if (searchvisible ===0) {
-        //Search is currently hidden. Slide down and show it.
-        $("#search-form").slideDown(200);
-        $("#s").focus(); //Set focus on the search input field.
-        searchvisible = 1; //Set search visible flag to visible.
-    } 
-
-    else {
-        //Search is currently showing. Slide it back up and hide it.
-        $("#search-form").slideUp(200);
-        searchvisible = 0;
-    }
-});
+// $("#search-menu").click(function(e){ 
+//     //This stops the page scrolling to the top on a # link.
+//     e.preventDefault();
+//
+//     var val = $('#search-icon');
+//     if(val.hasClass('ion-ios-search-strong')){
+//         val.addClass('ion-ios-close-empty');
+//         val.removeClass('ion-ios-search-strong');
+//     }
+//     else{
+//          val.removeClass('ion-ios-close-empty');
+//         val.addClass('ion-ios-search-strong');
+//     }
+//
+//
+//     if (searchvisible ===0) {
+//         //Search is currently hidden. Slide down and show it.
+//         $("#search-form").slideDown(200);
+//         $("#s").focus(); //Set focus on the search input field.
+//         searchvisible = 1; //Set search visible flag to visible.
+//     }
+//
+//     else {
+//         //Search is currently showing. Slide it back up and hide it.
+//         $("#search-form").slideUp(200);
+//         searchvisible = 0;
+//     }
+// });
 
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
- * 
+ *
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
